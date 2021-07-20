4d847bf048cd78e4e5364dc55249115f97acacdd
hakulachen <hakulachen@tencent.com>
2021-07-20 10:50:11 +0800

    test: more data

--- tests/generate.cpp
+++ tests/generate.cpp
@@ -15,14 +15,18 @@ int main() {
 
   std::vector<int64_t> dataset;
   for (auto x = 0; x < MAX_EPOCHS; ++x) {
-    auto y1 = NormalPdf(x, 30, 10) * 1000;
-    auto y2 = NormalPdf(x, 100, 15) * 2000;
-    auto y3 = NormalPdf(x, 150, 10) * 1500;
+    auto y1 = NormalPdf(x, 30, 15) * 1000;
+    auto y2 = NormalPdf(x, 100, 20) * 2000;
+    auto y3 = NormalPdf(x, 160, 15) * 2000;
     dataset.push_back(round(y1 + y2 + y3));
   }
 
-  for (auto i = 0; i < 15; ++i) {
-    for (auto y : dataset) output_file << y << " ";
-    output_file << "\n";
+  for (auto i = 0; i < 2; ++i) {
+    for (auto j = 1; j <= 10; ++j) {
+      for (auto k = 0; k < 2; ++k) {
+        for (auto y : dataset) output_file << y / j << " ";
+        output_file << "\n";
+      }
+    }
   }
 }
