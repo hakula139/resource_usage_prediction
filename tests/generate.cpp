d8ef2753735da2a4035b97b1e252d7ff529e90a3
hakulachen <hakulachen@tencent.com>
2021-07-27 11:50:33 +0800

    test: modify test data

--- tests/generate.cpp
+++ tests/generate.cpp
@@ -13,18 +13,18 @@ int main() {
 
   std::ofstream output_file(OUTPUT_PATH);
 
-  std::vector<int64_t> dataset;
+  std::vector<double> dataset;
   for (auto x = 0; x < MAX_EPOCHS; ++x) {
     auto y1 = NormalPdf(x, 30, 15) * 1000;
-    auto y2 = NormalPdf(x, 100, 20) * 2000;
-    auto y3 = NormalPdf(x, 160, 15) * 2000;
-    dataset.push_back(round(y1 + y2 + y3));
+    auto y2 = NormalPdf(x, 90, 20) * 2000;
+    auto y3 = NormalPdf(x, 160, 15) * 3000;
+    dataset.push_back(y1 + y2 + y3);
   }
 
-  for (auto i = 0; i < 2; ++i) {
-    for (auto j = 1; j <= 10; ++j) {
-      for (auto k = 0; k < 2; ++k) {
-        for (auto y : dataset) output_file << y / j << " ";
+  for (auto i = 0; i < 5; ++i) {
+    for (auto j = 0; j < 3; ++j) {
+      for (auto k = 1; k <= 3; ++k) {
+        for (auto y : dataset) output_file << round(y / k) << " ";
         output_file << "\n";
       }
     }
